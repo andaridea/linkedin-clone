aa3c655a6584f08e51cdf03a9753eddecddc28e3
andaridea <deapuspita1796@gmail.com>
2024-04-30 07:46:14 +0700

    created schema getall, add and delete for follow collection

--- server/schema/follow.js
+++ server/schema/follow.js
@@ -1,3 +1,5 @@
+const Follow = require ("../models/follow")
+
 const typeDefs = `#graphql
   type User {
     _id: ID
@@ -8,20 +10,41 @@ const typeDefs = `#graphql
   }
 
   type Query {
-   
+    getFollows: [Follow]
+  }
+
+  input newFollow {
+    followingId: ID
+    followerId: ID
   }
 
   type Mutation {
-   
+    addFollow(newFollow: newFollow): Follow
+    deleteFollow(id: ID): String
   }
 `;
 
 const resolvers = {
     Query: {
-        
+        getFollows: async () => {
+            const follows = await Follow.getAll()
+            return follows
+        }
     },
     Mutation: {
-      
+        addFollow: async (_, args) => {
+            const {followingId, followerId} = args.newFollow
+            const newFollow = {followingId, followerId}
+    
+            const result = await Follow.addFollow(newFollow)
+            newFollow._id = result.insertedId 
+            return newFollow
+        },
+        deleteFollow: async (_, args) => {
+            const { id } = args
+            const result = await Follow.deleteFollow(id)
+            return result
+          }
     }
 }
 
